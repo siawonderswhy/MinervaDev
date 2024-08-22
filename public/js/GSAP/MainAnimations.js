f53d53101ce4fa3e253a19666fc4188d00445ac7
Siavash Asadi <itssiavash@gmail.com>
2024-08-22 20:43:20 +0330

    Improved SmoothScroll Created a component for text to split into words and show up from below

--- public/js/GSAP/MainAnimations.js
+++ public/js/GSAP/MainAnimations.js
@@ -1,9 +1,24 @@
-gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
+// Registration of GSAP Plugins
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother, SplitText)
 
+// Scroll Smoother for all Pages
 document.addEventListener("astro:page-load", () => {
   const content = document.getElementById("smooth-content")
   gsap.set(document.body, { height: content.clientHeight })
   ScrollSmoother.create({
-    smooth: 1.5,
+    smooth: 0.86,
+    smoothTouch: 0.2,
   })
+})
+
+// Split Lines of text with ID of #Quote
+var lineSplitElements = document.querySelectorAll("#LineSplit")
+var split = new SplitText(lineSplitElements, {
+  type: "words",
+})
+gsap.from(split.words, {
+  duration: 0.86,
+  y: "100%",
+  ease: "power4",
+  stagger: 0.186,
 })
