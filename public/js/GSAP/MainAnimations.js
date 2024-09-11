94371b314e722c6170098b84a4be5103703f1146
Siavash Asadi <itssiavash@gmail.com>
2024-09-11 15:52:08 +0330

    Text Rotation component added

--- public/js/GSAP/MainAnimations.js
+++ public/js/GSAP/MainAnimations.js
@@ -11,7 +11,6 @@ document.addEventListener("astro:page-load", () => {
   })
 })
 
-// Split Lines of text with ID of #Quote
 var lineSplitElements = document.querySelectorAll("#LineSplit")
 var split = new SplitText(lineSplitElements, {
   type: "words",
@@ -21,21 +20,119 @@ gsap.from(split.words, {
   y: "100%",
   ease: "power4",
   stagger: 0.086, //Time between each word
-  onComplete: function () {
-    split.revert() // Revert the split text after the animation is complete
-  },
 })
 
+// var charSplitElements = document.querySelectorAll(".rotating-text")
+// let tl = gsap.timeline({
+//   repeat: -1,
+//   defaults: { stagger: 0.086 },
+// })
+// let charSplit = new SplitText(charSplitElements, {
+//   type: "words, chars",
+//   tag: "span",
+//   charsClass: "char-inline",
+// })
 
+// charSplit.words.forEach((word) => {
+//   if (
+//     word.parentElement.parentElement == document.querySelector(".rotating-text")
+//   ) {
+//     console.log(word)
+//   }
+//   console.log(word)
+//   tl.from(
+//     word.childNodes,
+//     {
+//       y: 100,
+//       ease: "expo.out",
+//     },
+//     "<0.386"
+//   )
+//   tl.to(word.childNodes, {
+//     delay: 0.086,
+//     y: -110,
+//     ease: "expo.in",
+//   }),
+//     "<0"
+// })
 
-const words = [
-  "Catalog",
-  "Brochure",
-  "Business Card",
-  "Logo",
-  "Mascot",
-  "Teaser",
-  "Merchandise",
-  "Billboard",
-  "Website",
-]
+const length = document.querySelectorAll(".word").length
+const duration = 0.86
+const staggerDelay = 0.086
+
+let lastStaggerLength = null
+const anim = gsap.timeline({
+  repeat: -1,
+  delay: 1.5,
+})
+
+for (let i = 0; i < length; i++) {
+  const splitElem = document.querySelectorAll(".word")[i]
+  const splitted = new SplitText(splitElem, {
+    type: "lines, chars",
+    charsClass: "char-inline",
+    tag: "span",
+  })
+
+  if (i == 0) {
+    anim.add(`break${i}`)
+
+    // "Leave" animation with adjusted delay
+    anim.to(
+      splitted.chars,
+      {
+        y: "-110%",
+        duration: duration,
+        ease: "expo.out",
+        stagger: staggerDelay,
+      },
+      `break${i}`
+    )
+
+    lastStaggerLength = splitted.chars.length
+  } else if (i == length - 1) {
+    anim.add(`break${i}`)
+
+    // "Come in" animation
+    anim.from(
+      splitted.chars,
+      {
+        y: "150%",
+        duration: duration,
+        ease: "expo.out",
+        stagger: staggerDelay,
+      },
+      `break${i}-=${duration + lastStaggerLength * staggerDelay}`
+    )
+
+    lastStaggerLength = splitted.chars.length
+  } else {
+    anim.add(`break${i}`)
+
+    // "Come in" animation
+    anim.from(
+      splitted.chars,
+      {
+        y: "150%",
+        duration: duration,
+        ease: "expo.out",
+        stagger: staggerDelay,
+      },
+      `break${i}-=${duration + lastStaggerLength * staggerDelay}`
+    )
+
+    // "Leave" animation with adjusted delay
+    anim.to(
+      splitted.chars,
+      {
+        y: "-110%",
+        duration: duration,
+        ease: "expo.out",
+        stagger: staggerDelay,
+      },
+      `break${i}`
+    )
+
+    lastStaggerLength = splitted.chars.length
+  }
+}
